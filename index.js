457952759a79e39834b96b655805c86e2e6de864
Samir Hossain <samir@Samirs-MacBook-Air.local>
2025-06-17 15:00:23 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const fs = require("fs");
+const path = require("path");
+
+// Terminal old massages clear
+process.stdout.write('\x1Bc');
+
+// Quotes load
+
+const filePath = path.join(__dirname, "data", "quotes.json");
+const quotes = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+
+// Random quote
+
+const pickRandom = (arr) => arr[Math.floor(Math.random()*arr.length)];
+const quote = pickRandom(quotes);
+
+// Printing
+
+console.log("\n" + chalk.greenBright(quote) + "\n");
